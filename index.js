0a7a2a6da80e299c5485eb1cebca209978f31f95
Gee Law <me@geelaw.cc>
2016-06-05 03:33:14 +0800

    Addressed issue #1. Now the page asks the user to confirm for big files and refuses to compare extra-large files. Moreover, the algorithm now runs asynchronously so that there's less chance to block the user's browser / system.
    Implemented a new automation feature: producer of automation tools can use COM or whatever interop technology of browsers, as long as it supports executing JavaScript code, can call `setFileNames` and then `doCompare` to provide the file names and content to be compared. The page should display as if the user has selected the corresponding files and tapped "compare". Note: this automation feature does not check validity. The caller is responsible for verifying the parameters are correct and the content to be compared is not too large.

--- index.js
+++ index.js
@@ -31,6 +31,23 @@ function gotoState(state)
     }
 }
 
+function setFileNames(fileName1, fileName2)
+{
+    var picker1text = document.getElementById("file1-name");
+    var picker2text = document.getElementById("file2-name");
+    var picker1header = document.getElementById("file1-name-header");
+    var picker2header = document.getElementById("file2-name-header");
+    var pickerHeaders = document.getElementById("file-name-headers");
+    picker1text.innerText = (fileName1 ? fileName1 : "");
+    if (fileName1)
+        picker1header.innerText = fileName1;
+    picker2text.innerText = (fileName2 ? fileName2 : "");
+    if (fileName2)
+        picker2header.innerText = fileName2;
+    if (fileName1 && fileName2)
+        pickerHeaders.innerText = fileName1 + "\n<->\n" + fileName2;
+}
+
 function pickerChanged()
 {
     var picker1 = document.getElementById("file1");
@@ -39,19 +56,10 @@ function pickerChanged()
     var resetButton = document.getElementById("reset-button");
     compareButton.className = (picker1.value && picker2.value ? "" : "gl-hidden");
     resetButton.className = (picker1.value || picker2.value ? "" : "gl-hidden");
-    var picker1text = document.getElementById("file1-name");
-    var picker2text = document.getElementById("file2-name");
-    var picker1header = document.getElementById("file1-name-header");
-    var picker2header = document.getElementById("file2-name-header");
-    var pickerHeaders = document.getElementById("file-name-headers");
-    picker1text.innerText = (picker1.value ? picker1.files[0].name : "");
-    if (picker1.value)
-        picker1header.innerText = picker1.files[0].name;
-    picker2text.innerText = (picker2.value ? picker2.files[0].name : "");
-    if (picker2.value)
-        picker2header.innerText = picker2.files[0].name;
-    if (picker1.value && picker2.value)
-        pickerHeaders.innerText = picker1.files[0].name + "\n<->\n" + picker2.files[0].name;
+    setFileNames(
+        picker1.value ? picker1.files[0].name : undefined,
+        picker2.value ? picker2.files[0].name : undefined
+    );
 }
 function resetPickers()
 {
@@ -61,6 +69,7 @@ function resetPickers()
     picker2.value = null;
     pickerChanged();
 }
+
 function invokePicker(pickerName)
 {
     var picker = document.getElementById(pickerName);
@@ -69,11 +78,27 @@ function invokePicker(pickerName)
 
 function compareClick()
 {
+    const FileSizeWarning = 1024 * 1024;
+    const FileSizeError = 3 * 1024 * 1024;
     gotoState("busy");
     var picker1 = document.getElementById("file1");
     var picker2 = document.getElementById("file2");
     var file1 = picker1.files[0];
     var file2 = picker2.files[0];
+    if (file1.size > FileSizeError
+        || file2.size > FileSizeError)
+    {
+        alert("You have chosen a file that is too large (more than " + FileSizeError.toString() + " bytes) to be compared.");
+        gotoState("pickers");
+        return;
+    }
+    if ((file1.size > FileSizeWarning
+        || file2.size > FileSizeWarning)
+        && !confirm("You have chosen a large file. It might take longer to compare the files, during which your browser might get slower.\n\nDo you still want to compare them?"))
+    {
+        gotoState("pickers");
+        return;
+    }
     resetPickers();
     var reader1 = new FileReader();
     var reader2 = new FileReader();
@@ -103,17 +128,27 @@ function animateCall(func)
 {
     // Prevent blocking the UI thread.
     // The name comes from the original usage: animation.
+    // The old name is here because at this
+    // stage it is not necessary to refactor.
     window.setTimeout(func, 0);
 }
 
-function runDP(dp, lines1, lines2)
+function runDPAndContinue(dp, lines1, lines2, continuation)
 {
     dp.push(new Array(lines2.length + 1));
     dp[0][0] = { value: 0, src: null };
     for (var j = 1; j <= lines2.length; ++j)
         dp[0][j] = { value: 0, src: { i: 0, j: j - 1 } };
-    for (var i = 1; i <= lines1.length; ++i)
+    var i = 1;
+    var backupTitle = document.title;
+    var recursivelyPerformForLoop = function ()
     {
+        if (i > lines1.length)
+        {
+            document.title = backupTitle;
+            animateCall(continuation);
+            return;
+        }
         dp.push(new Array(lines2.length + 1));
         dp[i][0] = { value: 0, src: { i: i - 1, j: 0 } };
         for (var j = 1; j <= lines2.length; ++j)
@@ -133,7 +168,11 @@ function runDP(dp, lines1, lines2)
                 dp[i][j].src.j = j - 1;
             }
         }
-    }
+        // Note: do not use 100.0 here, which can confuse the user.
+        document.title = Math.round(i++ * 99.0 / lines1.length).toString() + "%";
+        animateCall(recursivelyPerformForLoop);
+    };
+    animateCall(recursivelyPerformForLoop);
 }
 
 function doCompare(str1, str2)
@@ -142,50 +181,51 @@ function doCompare(str1, str2)
     var lines1 = str1.split(newlineRegex);
     var lines2 = str2.split(newlineRegex);
     var dp = new Array();
-    runDP(dp, lines1, lines2);
-    
-    var resultWide = document.getElementById("result-wide");
-    while (resultWide.childElementCount > 1)
-        resultWide.removeChild(resultWide.lastChild);
-    var resultNarrow = document.getElementById("result-narrow");
-    while (resultNarrow.childElementCount > 1)
-        resultNarrow.removeChild(resultNarrow.lastChild);
-    
-    gotoState("result");
-    
-    var result = document.getElementById("result");
-    var resultStrcmp0 = document.getElementById("result-strcmp-0");
-    var resultTotDiff = document.getElementById("result-totally-different");
-    result.className = "";
-    resultStrcmp0.className = "gl-hidden";
-    resultTotDiff.className = "gl-hidden";
-    if (lines1.length == lines2.length && dp[lines1.length][lines2.length].value == lines1.length)
+    runDPAndContinue(dp, lines1, lines2, function ()
     {
-        resultStrcmp0.className = "";
-        result.className = "gl-hidden";
-        return;
-    }
-    if (lines1.length > 10 && lines2.length > 10 && dp[lines1.length][lines2.length].value / Math.min(lines1.length, lines2.length) < 0.25)
-    {
-        resultTotDiff.className = "";
-        result.className = "gl-hidden";
-        return;
-    }
-    
-    rows = new Array();
-    renderResult(result, dp, lines1, lines2, lines1.length, lines2.length, 0);
-    
-    var renderer;
-    animateCall(renderer = function ()
-    {
-        if (rows.length == 0)
-            return;
-        for (var i = Math.min(10, rows.length); i > 0; --i)
+        var resultWide = document.getElementById("result-wide");
+        while (resultWide.childElementCount > 1)
+            resultWide.removeChild(resultWide.lastChild);
+        var resultNarrow = document.getElementById("result-narrow");
+        while (resultNarrow.childElementCount > 1)
+            resultNarrow.removeChild(resultNarrow.lastChild);
+        
+        gotoState("result");
+        
+        var result = document.getElementById("result");
+        var resultStrcmp0 = document.getElementById("result-strcmp-0");
+        var resultTotDiff = document.getElementById("result-totally-different");
+        result.className = "";
+        resultStrcmp0.className = "gl-hidden";
+        resultTotDiff.className = "gl-hidden";
+        if (lines1.length == lines2.length && dp[lines1.length][lines2.length].value == lines1.length)
         {
-            var x = rows.pop();
-            resultWide.appendChild(createWideRow(x));
-            resultNarrow.appendChild(createNarrowRow(x));
+            resultStrcmp0.className = "";
+            result.className = "gl-hidden";
+            return;
         }
+        if (lines1.length > 10 && lines2.length > 10 && dp[lines1.length][lines2.length].value / Math.min(lines1.length, lines2.length) < 0.25)
+        {
+            resultTotDiff.className = "";
+            result.className = "gl-hidden";
+            return;
+        }
+        
+        rows = new Array();
+        renderResult(result, dp, lines1, lines2, lines1.length, lines2.length, 0);
+        
+        var renderer = function ()
+        {
+            if (rows.length == 0)
+                return;
+            for (var i = Math.min(10, rows.length); i > 0; --i)
+            {
+                var x = rows.pop();
+                resultWide.appendChild(createWideRow(x));
+                resultNarrow.appendChild(createNarrowRow(x));
+            }
+            animateCall(renderer);
+        };
         animateCall(renderer);
     });
 }
